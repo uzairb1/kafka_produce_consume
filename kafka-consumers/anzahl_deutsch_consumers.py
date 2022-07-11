0f522e6eb33827b824a9786d74f0838888672e40
Uzair <bhatti.uzair78@outlook.com>
2022-07-11 16:53:54 +0200

    Saving results

--- kafka-consumers/anzahl_deutsch_consumers.py
+++ kafka-consumers/anzahl_deutsch_consumers.py
@@ -1,6 +1,6 @@
 from kafka import KafkaProducer, KafkaConsumer, TopicPartition #type:ignore
 from json import loads
-
+from csv import writer
 
 consumer = KafkaConsumer(
     'deutsch',
@@ -10,10 +10,19 @@ consumer = KafkaConsumer(
      group_id=None,
      value_deserializer=lambda x: loads(x.decode('utf-8')))
 
+def append_list_as_row(file_name, list_of_elem):
+     with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
+
 count = 1
 count1 =1
 for message in consumer:
     count = count+1
     if message.value == "group_end":
         print("Anzahl Deutsch Edits pro Minute ist: " + str(count))
+        row_contents = ["anzahl bearbeitungen in minute", count]
+        append_list_as_row('deutsch_edits.csv', row_contents)#type:ignore
         count = 1
--- kafka-consumers/anzahl_global_consumer.py
+++ kafka-consumers/anzahl_global_consumer.py
@@ -1,7 +1,14 @@
 from kafka import KafkaProducer, KafkaConsumer, TopicPartition #type:ignore
 from json import loads
+from csv import writer
 
 
+def append_list_as_row(file_name, list_of_elem):
+     with open(file_name, 'a+', newline='') as write_obj:
+        # Create a writer object from csv module
+        csv_writer = writer(write_obj)
+        # Add contents of list as last row in the csv file
+        csv_writer.writerow(list_of_elem)
 consumer = KafkaConsumer(
     'global',
      bootstrap_servers=['localhost:9092'],
@@ -16,4 +23,6 @@ for message in consumer:
     count = count+1
     if message.value == "group_end":
         print("Anzahl Global Edits pro Minute ist: " + str(count))
+        row_contents = ["anzahl bearbeitungen in minute", count]
+        append_list_as_row('global_edits.csv', row_contents)#type:ignore
         count = 1
